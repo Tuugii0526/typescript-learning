6b5b7b24019c8b265aec6a17ef2953aa9b5e4786
Tuugii <namjildorjtuguldur1234@gmail.com>
2024-11-14 07:48:44 +0800

    typescript is tried

---
+++ euler-problems/utils/solution.ts
@@ -0,0 +1,38 @@
+export abstract class AbstractSolution {
+  abstract getProblemName(): string;
+  async run(outputType: "console" | "json"): Promise<void> {
+    const startDate = new Date();
+    let nodeConsole = console;
+    if (outputType === "console") {
+      console.log(`Running solution for ${this.getProblemName()}`);
+    } else if (outputType === "json") {
+      console = {
+        log: () => {},
+        warn: () => {},
+        error: () => {},
+      } as any;
+    }
+    const solution = await this.solve();
+    const endDate = new Date();
+    if (outputType === "console") {
+      console.log("SOLUTION:", solution);
+      console.log(`Solution took: ${+endDate - +startDate}ms`);
+    } else if (outputType === "json") {
+      nodeConsole.log(
+        JSON.stringify({
+          executionTimeMs: +endDate - +startDate,
+          solution: solution?.toString() ? solution?.toString() : `${solution}`,
+        })
+      );
+    }
+  }
+  protected abstract solve(): any;
+}
+export function RunSolution(constructor:new()=>any){
+    let type='console';
+    if(process.argv[2]==="json")
+    {
+        type="json";
+    }
+    new constructor().run(type)
+} 
